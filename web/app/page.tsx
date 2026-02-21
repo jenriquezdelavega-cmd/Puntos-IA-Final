9cbb1de616091bfbc6810d2e7f83c2d136e86beb
jenriquezdelavega-cmd <jenriquezdelavega@gmail.com>
2026-02-21 06:07:08 +0000

    act

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -550,7 +550,7 @@ export default function Home() {
             <div className="aspect-video rounded-2xl border border-white/30 bg-black/30 overflow-hidden shadow-[0_12px_40px_rgba(0,0,0,0.35)]">
               <iframe
                 className="h-full w-full"
-                src="https://player.vimeo.com/video/1165202097?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+                src="https://player.vimeo.com/video/1165062263?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
 <<<<<<< HEAD
                 title="PUNTO IA"
 =======
